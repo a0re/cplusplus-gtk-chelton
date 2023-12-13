4391905a779c99fe8942932db89455a67b47b02e
era <jvoong1517@gmail.com>
2023-12-14 00:34:53 +1100

    Updated Server.cpp & Server.h -> There should now be no issues when rendering the Gtk::Window & Creating the Server itself as it exist in a different Thread

--- src/Server.cpp
+++ src/Server.cpp
@@ -1,10 +1,5 @@
 #include <iostream>
-#include <arpa/inet.h>
-#include <netinet/in.h>
-#include <thread>
 #include "Server.h"
-#include "CookieClicker.h"
-
 
 Server::Server() : serverSocket(-1){
     set_title("Cookie Clicker: Server");
@@ -18,19 +13,30 @@ Server::Server() : serverSocket(-1){
     m_grid.set_margin(20);
     set_child(m_grid);
 
-    m_grid.attach(lblConnection, 0, 0, 1, 1);
-    m_grid.attach(lblIP, 0, 1, 1, 1);
-    m_grid.attach(lblPort, 0, 2, 1, 1);
-    m_grid.attach(lblClient, 0, 3, 1, 1);
-    m_grid.attach(btnBack, 0, 4, 1, 1);
+    m_grid.attach(btnBack, 0, 0, 1, 1);
+    m_grid.attach(lblConnection, 0, 1, 1, 1);
+    m_grid.attach(lblIP, 0, 2, 1, 1);
+    m_grid.attach(lblPort, 0, 3, 1, 1);
+    m_grid.attach(lblClient, 0, 4, 1, 1);
 
     // Connect signal handler for the Back button
     btnBack.signal_clicked().connect(sigc::mem_fun(*this, &Server::onBackButtonClicked));
 
-    // Start the server
-    startServer();
+
+
+    // Start the Server & Listen for Connections from Clients
+    // This is being run on separate thread since the server will be running in an infinite loop
+    // This is done so no issues occur with the Rendering the Gtkmm::Window
+    std::thread serverThread(&Server::listeningForClientConnection, this);
+    serverThread.detach();
 }
 
+void Server::runServer() {
+    startServer();
+    listeningForClientConnection();
+}
+
+// Method to create a Socket and Create the server
 void Server::startServer() {
     // Create a socket for the server
     serverSocket = socket(AF_INET, SOCK_STREAM, 0);
@@ -41,7 +47,7 @@ void Server::startServer() {
     }
 
     struct sockaddr_in serverAddress{};
-    std::memset(&serverAddress, 0, sizeof(serverAddress));
+    memset(&serverAddress, 0, sizeof(serverAddress));
     serverAddress.sin_family = AF_INET; // Set the address family to IPv4
     serverAddress.sin_addr.s_addr = htonl(INADDR_ANY); // Setting the IP address to any interface
     serverAddress.sin_port = htons(5050); // Setting the port Number
@@ -65,11 +71,14 @@ void Server::startServer() {
     // Update the ipAddress member variable
     ipAddress = ip;
 
-    // Print IP address and port & Update the label
+    // Print IP address and port & Update the label to Connect
     std::cout << "Server started. IP: " << ipAddress << ", Port: " << ntohs(serverAddress.sin_port) << std::endl;
     lblIP.set_text("IP Address: " + ipAddress);
+}
 
-    // Listen for connections from clients
+
+// Method to listen for client connections and accept them | This method will run in a separate thread since it will be running in an infinite loop
+void Server::listeningForClientConnection() {
     // Listen for connections from clients
     if (listen(serverSocket, 5) == -1) {
         perror("Error listening for connections");
@@ -77,10 +86,10 @@ void Server::startServer() {
     }
 
     while (true) {
-        // Accept a connection from a client
-        struct sockaddr_in clientAddress{};
-        socklen_t clientAddrLen = sizeof(clientAddress);
-        clientSocket = accept(serverSocket, (struct sockaddr *) &clientAddress, &clientAddrLen);
+        std:: cout << "Listening for connections..." << std::endl;
+        struct sockaddr_in clientAddress{}; // Client address information
+        socklen_t clientAddrLen = sizeof(clientAddress); // Length of the client address
+        clientSocket = accept(serverSocket, (struct sockaddr *) &clientAddress, &clientAddrLen); // Accept a new connection
 
         if (clientSocket == -1) {
             perror("Error accepting connection");
@@ -88,29 +97,28 @@ void Server::startServer() {
         }
 
         // Display client information and ping the client
-        char clientIp[INET_ADDRSTRLEN];
-        inet_ntop(AF_INET, &(clientAddress.sin_addr), clientIp, INET_ADDRSTRLEN);
-        std::cout << "Client connected. IP: " << clientIp << ", Port: " << ntohs(clientAddress.sin_port) << std::endl;
-        lblClient.set_text("Client: " + std::string(clientIp) + ":" + std::to_string(ntohs(clientAddress.sin_port)));
+        char clientIp[INET_ADDRSTRLEN]; // Buffer to store the client IP address
+        inet_ntop(AF_INET, &(clientAddress.sin_addr), clientIp, INET_ADDRSTRLEN); // Convert the client IP address to a human readable form
+        std::cout << "Client connected. IP: " << clientIp << ", Port: " << ntohs(clientAddress.sin_port) << std::endl; // Print the client IP address and port
+        lblClient.set_text("Client: " + std::string(clientIp) + ":" + std::to_string(ntohs(clientAddress.sin_port))); // Update the label to show the client IP address and port
 
         // Ping the connected client
         PingClient(clientSocket);
 
-        // Close the client socket (you may want to keep it open for further communication)
-        ::close(clientSocket);
     }
-
 }
-
-void Server::PingClient(int clientSocket) {
-    const char* pingResponse = "PING_RESPONSE";
-    send(clientSocket, pingResponse, strlen(pingResponse), 0);
-}
-
 
 // Destructor for the Server class
 Server::~Server() {
+    // Close the server socket if it's open
+    if (serverSocket != -1) {
+        ::close(serverSocket);
+    }
+}
 
+void Server::PingClient(int clientSocket) {
+    const char* pingResponse = "PING_RESPONSE"; // Response to send to the client
+    send(clientSocket, pingResponse, strlen(pingResponse), 0); // Send the response to the client
 }
 
 
@@ -122,54 +130,4 @@ void Server::onBackButtonClicked() {
 
 
 
-/*
-void Server::acceptClientConnections() {
-    while (true) {
-        struct sockaddr_in clientAddress{};
-        socklen_t clientAddrLen = sizeof(clientAddress);
-
-        int newClientSocket = accept(serverSocket, (struct sockaddr *) &clientAddress, &clientAddrLen);
-
-        if (newClientSocket == -1) {
-            perror("Error accepting client connection");
-            // Continue to listen for the next connection
-            continue;
-        }
-
-        // Display client information
-        char clientIp[INET_ADDRSTRLEN];
-        inet_ntop(AF_INET, &(clientAddress.sin_addr), clientIp, INET_ADDRSTRLEN);
-        std::cout << "Accepted connection from " << clientIp << ":" << ntohs(clientAddress.sin_port) << std::endl;
-        lblClient.set_text("Client: " + std::string(clientIp));
-
-        // Create a new thread to handle communication with the client
-        std::thread([this, newClientSocket]() {
-            while (true) {
-                char buffer[1024];
-                ssize_t bytesRead = recv(newClientSocket, buffer, sizeof(buffer), 0);
-
-                if (bytesRead > 0) {
-                    buffer[bytesRead] = '\0';
-                    std::string receivedMessage(buffer);
-
-                    // Handle received message (if needed)
-                    if (receivedMessage == "PING") {
-                        std::cout << "Ping received from client.\n";
-                        // Send a response back to the client
-                        PingClient(newClientSocket);
-                    } else {
-                        std::cerr << "Invalid message received from client.\n";
-                    }
-                } else {
-                    // Handle error or disconnection
-                    std::cerr << "Error receiving data from client.\n";
-                    break;
-                }
-            }
-
-            // Close the client socket when done
-            ::close(newClientSocket);
-        }).detach();
-    }
-}*/
 
--- src/Server.h
+++ src/Server.h
@@ -1,11 +1,13 @@
 #pragma once
 #include <gtkmm.h>
-#include <cstring>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <netdb.h>
 #include <unistd.h>
+#include <thread>
+
+
 
 class Server : public Gtk::Window {
 public:
@@ -17,25 +19,15 @@ protected:
     int clientSocket;
     std::string ipAddress;
 
+    void runServer();
     void startServer();
     void onBackButtonClicked();
-
-
-    // Refer to the screenshots between Max & Dan
-    // Sends Cookie Clicker data to the client for Client to load the game
     void PingClient(int clientSocket);
+    void listeningForClientConnection();
 
 
-    //Test Function between Client and Server
-    //TODO: listenForConnection, acceptClientConnection & updateConnectionLabel functions in theory can be combined into one function
-    //void listenForConnections(); // Listens for connections from clients
-    void acceptClientConnections(); // Accepts client connections
 
-    //void updateConnectionLabel(); // Updates the connection label
 
-    //void LoadGame(); // Load the game? I need to have the window shown in the Server window underneath the IP Address and Port labels
-
-    //void sendGameDataToClient();
     /* Layout of Window */
     Gtk::Grid m_grid;
     Gtk::Label lblConnection, lblIP, lblPort;
@@ -43,6 +35,15 @@ protected:
 
 
     Gtk::Button btnBack;
+
+    // Refer to the screenshots between Max & Dan
+    // Sends Cookie Clicker data to the client for Client to load the game
+    //void LoadGame(); // Load the game? I need to have the window shown in the Server window underneath the IP Address and Port labels
+
+    //void sendGameDataToClient();
+
+
+
 
 
 };
